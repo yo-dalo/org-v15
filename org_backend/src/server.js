6eb9b9cf83d89f6430d9f0122c2b9d8d5dba8676
Adarshkumarjhook <kumaradarsh00572@gmail.com>
2025-01-30 16:25:24 +0530

    update

--- org_backend/src/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-const config = require('./config/env');
-
-
-
-app.listen(config.port, () => {
-    console.log(`Server is running on http://localhost:${config.port}`);
-});
